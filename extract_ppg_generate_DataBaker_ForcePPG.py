8452021726539e0e93f1568149f7305411474805
hujk17 <hujk17@mails.tsinghua.edu.cn>
2021-01-26 15:24:10 +0800

    ForcePPG提取databaker和ljspeech CN-PPG完成, https://blog.csdn.net/u013625492/article/details/113178784

--- extract_ppg_generate_DataBaker_ForcePPG.py
+++ extract_ppg_generate_DataBaker_ForcePPG.py
@@ -37,26 +37,29 @@ assert hparams == audio_hparams
 MFCC_DIM = 39
 PPG_DIM = 218
 
-# in1
-STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
-# v2 
-# meta_path = 'wavs_inference_and_findA_and_calculateCE_list.txt'
-# v3
-meta_path = 'wavs_inference_and_findA_and_calculateCE_v3_list.txt'
 
-# in2: NN->PPG
-ckpt_path = './aishell1_ckpt_model_dir/aishell1ASR.ckpt-128000'
+# in 
+meta_path = '/ceph/dataset/BZNSYP/ProsodyLabeling/000001-010000.txt'
+# 超参数会指定采样率16k, 外部磁盘就不用降了
+wav_dir = '/ceph/dataset/BZNSYP/Wave'
 
-# out
-# v2
-# log_dir = os.path.join('inference_findA_CE_PPG_log_dir', STARTED_DATESTRING)
-# v3
-log_dir = os.path.join('inference_findA_CE_PPG_v3_log_dir', STARTED_DATESTRING)
-ppg_dir = os.path.join(log_dir, 'ppg')
-rec_wav_dir = os.path.join(log_dir, 'rec_wavs_16000')
+# out1
+ppg_dir = './CN-PPG_databaker/ppg_ForcePPG'
+mfcc_dir = './CN-PPG_databaker/mfcc_ForcePPG'
+mel_dir = './CN-PPG_databaker/mel_ForcePPG'
+spec_dir = './CN-PPG_databaker/spec_ForcePPG'
+rec_wav_dir = './CN-PPG_databaker/rec_wavs_ForcePPG'
 os.makedirs(ppg_dir, exist_ok=True)
+os.makedirs(mfcc_dir, exist_ok=True)
+os.makedirs(mel_dir, exist_ok=True)
+os.makedirs(spec_dir, exist_ok=True)
 os.makedirs(rec_wav_dir, exist_ok=True)
+# out2
+out_meta_path = './CN-PPG_databaker/meta_list_ForcePPG.txt'
 
+
+# NN->PPG
+ckpt_path = './aishell1_ckpt_model_dir/aishell1ASR.ckpt-128000'
 
 
 def check_ppg(ppg):
@@ -65,13 +68,18 @@ def check_ppg(ppg):
 
 
 
-
-
-
-
 def main():
-    print('start...')
+    #这一部分用于处理DataBaker格式的数据集
     a = open(meta_path, 'r').readlines()
+    b = []
+    i = 0
+    while i < len(a):
+        b.append(a[i].strip()[:6])
+        i += 2
+    a = b
+    print(a[:3])
+
+    # a = PPG_get_restore(a, ppg_dir, ppg_dir, mel_dir, spec_dir)
 
     # NN->PPG
     # Set up network
@@ -86,14 +94,14 @@ def main():
     print('Restoring model from {}'.format(ckpt_path))
     saver.restore(sess, ckpt_path)
 
-    
+
+    meta_list_str = ''
     cnt = 0
     bad_list = []
-    for wav_f in tqdm(a):
+    for fname in tqdm(a):
         try:
-            wav_f = wav_f.strip()
-            print('process:', wav_f)
             # 提取声学参数
+            wav_f = os.path.join(wav_dir, fname + '.wav')
             wav_arr = load_wav(wav_f)
             mfcc_feats = wav2unnormalized_mfcc(wav_arr)
             ppgs = sess.run(predicted_ppgs, feed_dict={mfcc_pl: np.expand_dims(mfcc_feats, axis=0)})
@@ -101,8 +109,8 @@ def main():
             mel_feats = wav2normalized_db_mel(wav_arr)
             spec_feats = wav2normalized_db_spec(wav_arr)
 
-            # /datapool/home/hujk17/ppg_decode_spec_10ms_sch_Multi/inference_findA_Multi_log_dir/2020-11-09T11-09-00/0_sample_spec.wav
-            fname = wav_f.split('/')[-1].split('.')[0]
+            # 验证声学参数提取的对
+            save_name = fname + '.npy'
             save_mel_rec_name = fname + '_mel_rec.wav'
             save_spec_rec_name = fname + '_spec_rec.wav'
             assert ppgs.shape[0] == mfcc_feats.shape[0]
@@ -111,13 +119,21 @@ def main():
             write_wav(os.path.join(rec_wav_dir, save_spec_rec_name), normalized_db_spec2wav(spec_feats))
             check_ppg(ppgs)
             
-            # 存储ppg参数
-            ppg_save_name = os.path.join(ppg_dir, fname + '.npy')
+            # 存储声学参数
+            mfcc_save_name = os.path.join(mfcc_dir, save_name)
+            ppg_save_name = os.path.join(ppg_dir, save_name)
+            mel_save_name = os.path.join(mel_dir, save_name)
+            spec_save_name = os.path.join(spec_dir, save_name)
+            np.save(mfcc_save_name, mfcc_feats)
             np.save(ppg_save_name, ppgs)
+            np.save(mel_save_name, mel_feats)
+            np.save(spec_save_name, spec_feats)
 
+            meta_list_str += fname + '\n'
             cnt += 1
+            print(fname + 'success...')
         except Exception as e:
-            bad_list.append(wav_f)
+            bad_list.append(fname)
             print(str(e))
         
         # break
@@ -125,6 +141,9 @@ def main():
     print('good:', cnt)
     print('bad:', len(bad_list))
     print(bad_list)
+
+    f_meta = open(out_meta_path, 'w')
+    f_meta.write(meta_list_str)
 
     return
 
